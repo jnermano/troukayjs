8815e397ee052a4b971f2d34003a3538a1d7ecea
Ermano Nicolas Joseph <jnermano86@gmail.com>
2019-11-13 15:05:17 -0500

    design play 2 player color indication

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 var holes = [];
-var currentPlayer = 1;
+var current_hole;
+var start_hole;
+
+var currentPlayer = 0;
+var users = [];
+
 var stonesInHand = 0;
-let speed = 1000 * 1;
+let speed = 1000 * 0.1;
 var inProgress = false;
 var gameLoop;
 
@@ -22,11 +27,16 @@ function play(holeDOMId){
     let holeNumber = parseInt(holeDOMId.split("-").pop());
     hole = holes[holeNumber - 1];
 
-    if(holeNumber.stones == 0)
+    if(hole.stones == 0){
+        inProgress = false;
         return;
+    }
 
-    stonesInHand = hole.stones;
+    users[currentPlayer].inHand = hole.stones;
     hole.stones = 0;
+
+    start_hole = hole;
+    $("#hole-"+start_hole.id).addClass("start");
     
     updateBoard();
 
@@ -42,19 +52,41 @@ function play(holeDOMId){
         console.log(`counter: ${counter} | ${counter%9}`);
         h = holes[counter];
 
+        if(current_hole != null){
+            $("#hole-"+current_hole.id).removeClass("active");
+        }
+    
+        current_hole = h;
+        $("#hole-"+current_hole.id).addClass("active");
+
         h.stones += 1;
-        stonesInHand -= 1;
+        users[currentPlayer].inHand -= 1;
 
         updateBoard();
 
-        if(stonesInHand == 0 && h.stones > 1){
-            stonesInHand = h.stones;
+        if(h.stones == 5){
+            users[currentPlayer].earnigs += h.stones;
+            h.stones = 0;
+        }
+
+        if(users[currentPlayer].inHand == 0 && h.stones > 1){
+            users[currentPlayer].inHand = h.stones;
             h.stones = 0;
             updateBoard();
         }
 
-        if(stonesInHand == 0)
+
+        if(users[currentPlayer].inHand == 0){
             clearInterval(gameLoop);
+            inProgress = false;
+            $("#hole-"+current_hole.id).removeClass("active");
+            $("#hole-"+start_hole.id).removeClass("start");
+            
+            currentPlayer = currentPlayer == 0? 1 : 0;
+
+            updateBoard();
+        }
+            
     }, 
     speed);
 
@@ -65,6 +97,21 @@ function updateBoard(){
         if(hole.active){
             $(`#nb_stones_hole_${hole.id}`).text(hole.stones);
         }
+    });
+
+    $(`#current_player`).text(currentPlayer + 1);
+
+    
+    $(`#player-${currentPlayer == 0? 2: 1}`).removeClass("playing");
+    $(`#player-${currentPlayer == 0? 1: 2}`).addClass("playing");
+
+    updateGameStates();
+}
+
+function updateGameStates(){
+    users.forEach(user => {
+        $(`#player-${user.id}-earnigs`).text(user.earnigs);
+        $(`#player-${user.id}-hands`).text(user.inHand);
     });
 }
 
@@ -82,6 +129,21 @@ function resetGame(){
         {id: 8, stones: 5, active: true},
         {id: 9, stones: 5, active: true},
         {id: 10, stones: 5, active: true},
+    ];
+
+    users = [
+        {
+            id: 1,
+            name: "Player 1",
+            earnigs: 0,
+            inHand: 0
+        },
+        {
+            id: 2,
+            name: "Player 2",
+            earnigs: 0,
+            inHand: 0
+        }
     ];
 
     updateBoard();
